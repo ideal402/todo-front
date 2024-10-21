e10d0372d5e4950152e61e13f40babe094dc7812
been <ideal402@naver.com>
2024-10-21 12:02:51 +0900

    [feat] 프라이빗 라우터, 토큰으로 유저정보 가져오기 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import TodoPage from "./pages/TodoPage";
 import RegisterPage from "./pages/RegisterPage";
+import PrivateRoute from "./route/PrivateRoute";
+import api from "./utils/api"
 
 function App() {
+  const [user, setUser] = useState(null);
+  
+  const getUser = async () =>{
+    try{
+      const storedToken = sessionStorage.getItem("token");
+      if(storedToken){
+        const response = await api.get("/user/verify");
+        console.log("🚀 ~ getUser ~ response:", response)
+      }    
+    }catch(error){
+
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  },[])
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TodoPage />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute user = {user}>
+              <TodoPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="/register" element={<RegisterPage />} />
-
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
